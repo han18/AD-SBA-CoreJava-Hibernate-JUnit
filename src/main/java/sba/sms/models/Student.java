9f2e7c61e11e264495bc4298615f3d240628f2f3
han18 <han.fadah@gmail.com>
2024-12-20 18:10:03 -0500

    pushing partial code

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -8,6 +8,53 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter@Getter@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "student")
+@Entity
+public class Student {
+    @NonNull
+    @Id @Column(length = 50,name = "email")
+    String email;
+    @NonNull
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+    @NonNull
+    @Column(length = 50,name = "password")
+    String password;
+
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @JoinTable(name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_email"),
+            inverseJoinColumns = @JoinColumn(name = "courses_id"))
+    Set<Course> courses = new LinkedHashSet<>();
+
+    public void addCourse(Course c){
+        courses.add(c);
+        c.getStudents().add(this);
+
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return email.equals(student.email) && name.equals(student.name) && password.equals(student.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, name, password);
+    }
+}
+
 
 /**
  * Student is a POJO, configured as a persistent class that represents (or maps to) a table
@@ -16,11 +63,3 @@ import java.util.Set;
  * data. The Student class can be viewed as the owner of the bi-directional relationship.
  * Implement Lombok annotations to eliminate boilerplate code.
  */
-
-public class Student {
-
-
-    }
-
-
-
